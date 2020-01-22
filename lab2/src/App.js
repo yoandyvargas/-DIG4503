203b188e81b9823cde8f97559d2bf30100fbb02d
yoandyvargas <46614370+yoandyvargas@users.noreply.github.com>
2020-01-22 18:37:48 -0500

    Update App.js
    Modified function into class

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "./components/HomePage/index.js";
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage firstName="Yoandy" />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage firstName="Yoandy" />
+      </div>
+    );
+  }
 }
 
 export default App;
