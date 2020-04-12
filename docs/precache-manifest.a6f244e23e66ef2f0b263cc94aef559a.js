4049e69c807bf029a5fd91a46cc1548e8fa67d5e
yoandyvargas <46614370+yoandyvargas@users.noreply.github.com>
2020-04-12 17:43:55 -0400

    Docs Upload

---
+++ docs/precache-manifest.a6f244e23e66ef2f0b263cc94aef559a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7762c1cc7f0ab892a7ce93374c947a0",
+    "url": "/DIG4503/index.html"
+  },
+  {
+    "revision": "d69cc9a678f8266601c5",
+    "url": "/DIG4503/static/css/main.5d705d22.chunk.css"
+  },
+  {
+    "revision": "4b4096fb297944f19491",
+    "url": "/DIG4503/static/js/2.8bd7cbb7.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/DIG4503/static/js/2.8bd7cbb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69cc9a678f8266601c5",
+    "url": "/DIG4503/static/js/main.71490d6e.chunk.js"
+  },
+  {
+    "revision": "c17a19c3715532cfb318",
+    "url": "/DIG4503/static/js/runtime-main.eae88ffd.js"
+  }
+]);
